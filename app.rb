5681ea8357843ec5a5eb983d74aed756c2dc54f1
Randy Steele <steelerx155@gmail.com>
2020-01-06 19:15:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,8 +7,15 @@ class App < Sinatra::Base
    erb :newteam
   end
   
-  post "/newteam" do 
-    :name
+  post "/team" do 
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:point_guard]
+    @sg = params[:shoot_guard]
+    @pf = params[:power_for]
+    @sf = params[:shoot_for]
+    @c = params[:cen]
+    erb :team
   end
   
  
